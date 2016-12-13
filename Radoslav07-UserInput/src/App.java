d9c5f0fe48f6806089b02810486d6b034c7c93da
radoslav.latincic <latincic@teol.net>
2016-12-13 14:20:28 +0100

    Dodavanje novih primjera

--- Radoslav07-UserInput/src/App.java
+++ Radoslav07-UserInput/src/App.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+String ime =new String();
+String prezime =new String();
+String pol =new String();
+
+System.out.println("Unesite svoje ime:");
+ime=input.nextLine();
+System.out.println("Unesite svoje prezime:");
+prezime=input.nextLine();
+System.out.println("Unesite pol:");
+pol=input.nextLine();
+System.out.println("Zdravo "+ime+" "+prezime+"."+"Vi ste "+pol+".");
 
 	}
 
